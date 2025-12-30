b0b683458223a2fa40950c4d4eca845505222226
th3gh0s8 <chamudithapasindu54@gmail.com>
2025-12-30 23:56:29 +0530

    debug adapter was updated

--- lua/config/plugins/debugging.lua
+++ lua/config/plugins/debugging.lua
@@ -7,6 +7,7 @@ return {
             "theHamsta/nvim-dap-virtual-text",
             "nvim-neotest/nvim-nio",
             "williamboman/mason.nvim",
+            "jay-babu/mason-nvim-dap.nvim",
         },
         config = function()
             local dap = require("dap")
@@ -14,6 +15,30 @@ return {
 
             require("dapui").setup()
             require("dap-go").setup()
+            require("mason").setup()
+
+            require("mason-nvim-dap").setup({
+                ensure_installed = { "stylua", "jq" },
+                handlers = {
+                    function(config)
+                        -- all sources with no handler get passed here
+
+                        -- Keep original functionality
+                        require("mason-nvim-dap").default_setup(config)
+                    end,
+                    --[[ python = function(config)
+                        config.adapters = {
+                            type = "executable",
+                            command = "/usr/bin/python3",
+                            args = {
+                                "-m",
+                                "debugpy.adapter",
+                            },
+                        }
+                        require("mason-nvim-dap").default_setup(config) -- don't forget this!
+                    end, ]]
+                },
+            })
 
             require("nvim-dap-virtual-text").setup({
                 -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
