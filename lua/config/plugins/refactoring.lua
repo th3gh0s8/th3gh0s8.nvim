c5ed5307169edafe21d54068aa3da9228c9c8e45
Chamuditha Pasindu <chamudithapasindu54@gmail.com>
2026-05-21 11:43:00 +0530

    updates

--- lua/config/plugins/refactoring.lua
+++ lua/config/plugins/refactoring.lua
@@ -1,12 +1,13 @@
-  return{
+return {
     "ThePrimeagen/refactoring.nvim",
-      config = function()
-        require('refactoring').setup({})
+    config = function()
+        require("refactoring").setup({})
     end,
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "lewis6991/async.nvim",
     },
     lazy = false,
     opts = {},
-  }
+}
